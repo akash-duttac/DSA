118d465f86d98da33c14a76f04ebfd65e05d4b64
akash-duttac <105050803+akash-duttac@users.noreply.github.com>
2024-07-25 02:22:48 +0530

    sorting algos

---
+++ selection_sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        for (int j=i+1; j<n; j++){
+            if (arr[j]<arr[i]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin>>arr[i];
+
+    selection_sort(arr,n);
+    for (int i=0; i<n; i++)
+        cout<<arr[i]<<" "<<endl;
+
+    
+return 0;
+}
